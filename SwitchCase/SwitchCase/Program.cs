744cc5210fc5f59f161a4518a64b971019bf5275
Thiago K <thiagokovalski5@gmail.com>
2024-10-17 02:06:39 -0300

    Switch Case e Funções Básicas para uma Calculadora

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static double Soma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static double Sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        static double Mult(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        static double Div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo a calculadora do Thig0S!\n");
+            while (true) { 
+                Console.Write("Digite o primeiro número: ");
+                double num1 = Double.Parse(Console.ReadLine());
+
+                Console.Write("\nDigite o segundo número: ");
+                double num2 = Double.Parse(Console.ReadLine());
+
+                Console.Write("\nSoma: 1");
+                Console.Write("\nSubtração: 2");
+                Console.Write("\nMultiplicação: 3");
+                Console.Write("\nDivisão: 4");
+                Console.Write("\nDigite o método desejado: ");
+                int opcao = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                double resultado;
+                switch (opcao) {
+                    case 1:
+                        resultado = Soma(num1, num2);
+                        Console.WriteLine("");
+                        Console.WriteLine($"O resultado da SOMA é: {resultado}");                     
+                        Console.WriteLine("");
+                        break;
+                    case 2:
+                        resultado = Sub(num1, num2);
+                        Console.WriteLine("");
+                        Console.WriteLine($"O resultado da SUBTRAÇÃO é: {resultado}");
+                        Console.WriteLine("");
+                        break;
+                    case 3:
+                        resultado = Mult(num1, num2);
+                        Console.WriteLine("");
+                        Console.WriteLine($"O resultado da MULTIPLICAÇÃO é: {resultado}");
+                        Console.WriteLine("");
+                        break;
+                    case 4:
+                        resultado = Div(num1, num2);
+                        Console.WriteLine("");
+                        Console.WriteLine($"O resultado da DIVISÃO é: {resultado}");
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine("Digite um número Válido!");
+                        break;
+                }
+                Console.WriteLine("Deseja continuar? (s/n)");
+                string continuar = Console.ReadLine();
+                if (continuar.ToLower() != "s") break;
+                Console.Clear();
+            }
+        }
+    }
+    
+}
---
+++ SwitchCase/SwitchCase/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("SwitchCase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwitchCase")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("237132c3-0085-4e2a-8a53-a3864e4f2305")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
