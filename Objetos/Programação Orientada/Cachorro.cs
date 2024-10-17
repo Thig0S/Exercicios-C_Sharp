79c3a139fb3b126b692502a662a0f439d7c41194
Thiago K <thiagokovalski5@gmail.com>
2024-10-17 03:16:37 -0300

    Programação Orientada a Objetos-Ex.Básico

---
+++ Objetos/Programação Orientada/Cachorro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programação_Orientada
+{
+    internal class Cachorro
+    {
+        private string nome;
+        private int idade;
+        private bool sono;
+
+        public string Nome { get { return nome; } set { nome = value; } }
+        public int Idade { get { return idade; } set { idade = value; } }
+        public bool Sono { get { return sono; } set { sono = value; } }
+
+        public void MostrarCachorro()
+        {
+            Console.WriteLine($"\nO nome do cachorro/cachorra é {Nome} e possui {Idade} anos de vida!\n");
+            if (Sono)
+            {
+                Console.WriteLine("O Cachorro Precisa Dormir Coitado!\n");
+            }
+            else
+            {
+                Console.WriteLine("O cachorro não está com Sono\n");
+            }
+        }
+    }
+}
---
+++ Objetos/Programação Orientada/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programação_Orientada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int idade;
+                string nome;
+                Cachorro cachorro1 = new Cachorro();
+                do { 
+
+                    Console.Write("Digite o nome do cachorro: ");
+                    nome = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(nome)) {
+                        Console.Write("O nome do cachorro não pode estar Vazio," +
+                            "Digite um nome válido!\n");
+                    } 
+
+                cachorro1.Nome = nome;
+                    } while (string.IsNullOrWhiteSpace(nome));
+
+                Console.Write("\nDigite o idade do cachorro: ");
+                while (!int.TryParse(Console.ReadLine(), out idade))
+                {
+                    Console.Write("Entrada inválida. Digite um valor válido.");
+                }
+                cachorro1.Idade = idade;
+
+                Console.Write("O cachorro está com sono? (s/n)");
+                if(Console.ReadLine().ToLower() == "s")
+                {
+                    cachorro1.Sono = true;
+                }
+                Console.Clear();
+                cachorro1.MostrarCachorro();
+
+                Console.Write("\nDeseja Cadastrar Outro Cachorro? (s/n)");
+                if (Console.ReadLine().ToLower() != "s")
+                {
+                    break;
+                };
+            }
+        }
+    }
+}
