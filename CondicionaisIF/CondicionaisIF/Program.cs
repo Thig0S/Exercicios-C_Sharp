8f750cd38a26567cd068c23d244992ba06519096
Thiago K <thiagokovalski5@gmail.com>
2024-10-17 01:37:06 -0300

    Condicional IF Calcular Desconto

---
+++ CondicionaisIF/CondicionaisIF/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionaisIF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            double desconto;          
+
+            Console.WriteLine("====/Calculadora de Descontos/====\n");
+            Console.Write("Produtos acima de R$1350,00 recebem 12% de Desconto!\n");
+            
+            while (true)
+            {
+                Console.Write("Digite o valor do Produto: R$ ");
+                if (!Double.TryParse(Console.ReadLine(), out valor))
+                {
+                    Console.WriteLine("Digite um valor VÁLIDO!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(valor > 1350.00){
+                desconto = valor - (valor * 0.12);
+                Console.WriteLine("");
+                Console.WriteLine("Desconto de 12% aplicado!\n");
+                Console.WriteLine($"O valor final do produto é: R${desconto}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Nenhum desconto foi aplicado!");
+                Console.WriteLine($"O valor final do produto é: R${valor}");
+                Console.ReadLine();
+            }                       
+        }
+    }
+}
