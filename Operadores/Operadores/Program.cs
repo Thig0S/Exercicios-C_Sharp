67b01a4f6201f2df75c723985119645347c84264
Thiago K <thiagokovalski5@gmail.com>
2024-10-17 01:17:51 -0300

    Operadores Matemáticos

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            double num1 = Double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double num2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A soma dos números é: {num1 + num2}");
+            Console.WriteLine($"A subtração dos números é: {num1 - num2}");
+            Console.WriteLine($"A multiplicação dos números é: {num1 * num2}");
+            Console.WriteLine($"A divisão dos números é: {num1 / num2}");
+            Console.ReadLine();
+        }
+    }
+}
