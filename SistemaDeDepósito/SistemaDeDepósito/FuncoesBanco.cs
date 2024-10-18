3b073ba4a248d897798e3ccd85d534fabe4e4659
Thiago K <thiagokovalski5@gmail.com>
2024-10-18 00:47:32 -0300

    Programação Orientada a Objetos Ex2

---
+++ SistemaDeDepósito/SistemaDeDepósito/FuncoesBanco.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class ContaBancaria
+{
+    private double Saldo;
+    private double ValorInserito;
+
+    public ContaBancaria()
+    {
+        Saldo = 0; 
+    }
+
+    public void ExibirSaldo()
+    {
+        Console.WriteLine($"O seu saldo é R$:{Saldo}");
+        Console.WriteLine("Pressione ENTER para continuar");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    
+    public void Saque(double valorSaque)
+    {
+        if (valorSaque < 0 && valorSaque < Saldo)
+        {
+            Saldo -= ValorInserito;
+
+            Console.WriteLine($"\nSaque de R${ValorInserito} executado com Sucesso!");
+            Console.WriteLine($"Seu novo saldo é R$:{Saldo}\n");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadLine();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"Valor Inválido! O saque deve ser maior que zero e menor que o seu Saldo: {Saldo}!");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+    public void Deposito(double valorDeposito)
+    {
+        Saldo += valorDeposito; 
+
+        Console.WriteLine($"\nDepósito executado com Sucesso!");
+        Console.WriteLine($"Seu novo saldo é R$:{Saldo}\n");
+        Console.WriteLine("Pressione ENTER para continuar");
+        Console.ReadLine();
+        Console.Clear();
+    }
+}
---
+++ SistemaDeDepósito/SistemaDeDepósito/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeDepósito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria();
+            while (true)
+            {
+                int opcao = 0;
+                bool opcaoValida = false;
+
+                Console.WriteLine("==|Bem-vindo ao Banco TK|==\n");
+
+                while (!opcaoValida)
+                {
+                    Console.WriteLine("1 - Para ver seu saldo");
+                    Console.WriteLine("2 - Para fazer um saque");
+                    Console.WriteLine("3 - Para fazer um depósito");
+                    Console.WriteLine("4 - Para encerrar o programa\n");
+                    Console.Write("Digite o método desejado: ");
+
+                    try
+                    {
+                        opcao = int.Parse(Console.ReadLine());
+
+                        if (opcao >= 1 && opcao <= 4)
+                        {
+                            opcaoValida = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nOpção inválida. Por favor, escolha um número entre 1 e 4.\n");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("\nEntrada inválida. Digite um número.\n");
+                    }
+                }
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1: // Ver saldo
+                        contaBancaria.ExibirSaldo();
+                        break;
+
+                    case 2: // Fazer saque
+                        Console.Write("Digite o valor do Saque R$: ");
+                        double valorSaque = double.Parse(Console.ReadLine());
+                        contaBancaria.Saque(valorSaque); 
+                        break;
+
+                    case 3: // Fazer depósito
+                        Console.Write("Digite o valor do Depósito R$: ");
+                        double valorDeposito = double.Parse(Console.ReadLine());
+                        contaBancaria.Deposito(valorDeposito);
+                        break;
+
+                    case 4: // Sair
+                        Console.WriteLine("Obrigado por utilizar o Banco TK. Até logo!");
+                        return; 
+                }
+            }
+        }
+    }
+}
