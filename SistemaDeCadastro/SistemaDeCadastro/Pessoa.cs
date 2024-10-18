b43b86717d0157754fe2df96425e50dd56e87244
Thiago K <thiagokovalski5@gmail.com>
2024-10-18 03:15:19 -0300

    Sistema básico de cadastro com POO para trabalhar com variáveis privadas

---
+++ SistemaDeCadastro/SistemaDeCadastro/Pessoa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeCadastro
+{
+    class Pessoa
+    {
+        private string nome;
+        private int idade;
+ 
+        public void DefinirNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void ValidarIdade(int idade)
+        {
+            while (true)
+            {
+                try
+                {
+                    idade = int.Parse(Console.ReadLine());
+                    if (idade > 0)
+                    {
+                        DefinirIdade(idade);
+                        break;
+                    }
+                    else
+                    { //Criar um error para o chatch caso o numero seja menor que 0
+                        throw new ArgumentOutOfRangeException(nameof(idade), "A idade deve ser maior que 0!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Digite um número válido! ");
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    //Provavélmente não precisava deste outro catch
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public void DefinirIdade(int idade)
+        {
+            this.idade = idade;   
+        }
+        public void MostrarNomeIdade()
+        {
+            Console.WriteLine($"Nome: {this.nome} e idade: {this.idade}");
+            Console.ReadLine();
+            Console.Clear();
+        }     
+    }
+}
---
+++ SistemaDeCadastro/SistemaDeCadastro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeCadastro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            while (true)
+            {
+
+                //Atividade para melhorar o uso de variavéis privadades dentro da Classe Pessoa.cs       
+
+                Console.WriteLine("===|Cadastro TK|===");
+
+                Console.WriteLine("Digite o NOME da pessoa a ser cadastrada");
+                string nome = Console.ReadLine();
+                pessoa.DefinirNome(nome);
+
+                Console.WriteLine("Digite a IDADE da pessoa a ser cadastrada");
+                int idade = 0;
+                pessoa.ValidarIdade(idade);
+
+                pessoa.MostrarNomeIdade();
+                if (Console.ReadLine().ToLower() != "s")
+                {
+                    break; // Sai do loop se a resposta não for 's'
+                }
+
+            }
+        }
+    }
+}
